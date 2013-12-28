eac2be633ade1fa1b3dfcff8c6debba1cabcc6c6
giteup <dougharman@Douglass-MacBook-Pro.local>
2013-12-27 21:55:29 -0600

    2013_12_27 - Today's work 9:55pm

--- FRED_Mashup.js
+++ FRED_Mashup.js
@@ -1,89 +1,51 @@
+ google.load("feeds", "1");
 
-/*
-
-  google.load("feeds", "1");
-
-      function initialize() {
-        var feed = new google.feeds.Feed("http://feeds.feedburner.com/stlfed-fred-select");
-        feed.setNumEntries(6);
-        feed.load(function(result) {
-          if (!result.error) {
-            var container = document.getElementById("feed");
-            for (var i = 0; i < result.feed.entries.length; i++) {
-              var entry = result.feed.entries[i];
-              var div = document.createElement("div");
-              div.appendChild(document.createTextNode(entry.title));
-              container.appendChild(div);
-            }
-          }
-        });
+function initialize() {
+  var feed = new google.feeds.Feed("http://feeds.feedburner.com/stlfed-fred-select");
+  feed.setNumEntries(6);
+  feed.load(function(result) {
+    if (!result.error) {
+      var container = document.getElementById("feed");
+      for (var i = 0; i < result.feed.entries.length; i++) {
+        var entry = result.feed.entries[i];
+        var div = document.createElement("div");
+        div.appendChild(document.createTextNode(entry.title));
+        container.appendChild(div);
       }
-
-  google.setOnLoadCallback(initialize);
-
-var successFunc = function (resp) {
- $('#output').html(data.contents);
-  console.log(resp);
-}
-*/
-/*  success: function( data ) {
-    var items = [];
-    $.each( data, function( key, val ) {
-      items.push( "<li id='" + key + "'>" + val + "</li>" );
-    });
-   
-    $( "<ul/>", {
-      "class": "my-new-list",
-      html: items.join( "" )
-    }).appendTo( "body" );
-  },
-  error: printError  */
-
-/*
-var successFunc = function(resp) {
-  console.log(resp.contents);
-}
-
-var printError = function( req, status, err ) {
-  console.log( 'something went wrong', status, err );
-};
-*/   
-/*
-$.ajax({
-  url: "http://anyorigin.com/get?url=http%3A//api.stlouisfed.org/fred/release%3Frelease_id%3D53%26api_key%3De19770641c3d3ed9c68f7cd06544bcfb%26file_type%3Djson%26callback%3D%3F&callback=?",
-  dataType: "json",
-  success: successFunc,
-  error: printError
-});
-
-
-/*  */
-
-$(document).ready(function(){
-
-  $(".button2, #retrieve").keydown(function (event) {
-  if (event.which == 13) {
-  event.preventDefault();
-  }
-});
-// Initiate the request!
-
- $.ajax({
-  url:'http://anyorigin.com/get?url=http%3A//api.stlouisfed.org/fred/release%3Frelease_id%3D53%26api_key%3De19770641c3d3ed9c68f7cd06544bcfb%26file_type%3Djson%26callback%3D&callback=?', 
-    dataType: "json",
-    success: function(data){$('#output').html(data.releases)}
-});
-
-
-/*  $(".button2, #retrieve").click(function () {
-    event.preventDefault();
+    }
   });
-*/
+}
 
-$.getJSON('http://anyorigin.com/get?url=http%3A//api.stlouisfed.org/fred/release%3Frelease_id%3D53%26api_key%3De19770641c3d3ed9c68f7cd06544bcfb%26file_type%3Djson%26callback%3D&callback=?', 
-  function(data){
-  $('#output').html(data.contents);
+google.setOnLoadCallback(initialize);
+
+
+
+
+$(document).ready(function() {
+
+
+ $.getJSON('http://anyorigin.com/get?url=http%3A//api.stlouisfed.org/fred/series/release%3Fseries_id%3DIRA%26api_key%3De19770641c3d3ed9c68f7cd06544bcfb%26file_type%3Djson&callback=?', 
+        function(data){
+        console.log(data.contents.releases[0].id);
+    });
+
+
+$.getJSON('http://anyorigin.com/get?url=http%3A//api.stlouisfed.org/fred/series/release%3Fseries_id%3DIRA%26api_key%3De19770641c3d3ed9c68f7cd06544bcfb%26file_type%3Djson&callback=?', 
+      function(data){
+      console.log(data.contents.releases[0].link);
+    });
+
+
+$.ajax({
+  url:'http://anyorigin.com/get?url=http%3A//api.stlouisfed.org/fred/series/search%3Fsearch_text%3Dconsumer+price+index%26api_key%3De19770641c3d3ed9c68f7cd06544bcfb%26file_type%3Djson&callback=?', 
+    dataType: "json",
+    success: function(data){console.log(data.contents.seriess[0].id)},
 });
+
+
+  
+
+
 
 
 });
